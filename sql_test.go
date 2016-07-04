1b4a59d2fc6ccde5d9929ac0b0e01dd3f05c74a5
Ox Cart <ox.to.a.cart@gmail.com>
2016-07-03 20:00:19 -0500

    Updated sql test

--- sql_test.go
+++ sql_test.go
@@ -12,8 +12,7 @@ func TestSQL(t *testing.T) {
 	aq := &Query{}
 	err := aq.applySQL(`
 SELECT AVG(a / (A + b + C)) * 2 AS rate
-FROM Table_A
-ASOF '-60m' UNTIL '-15m'
+FROM Table_A ASOF '-60m' UNTIL '-15m'
 WHERE Dim_a LIKE '172.56.' AND (dim_b > 10 OR dim_c = 20) OR dim_d != 'thing'
 GROUP BY dim_A, period('5s') // period is a special function
 HAVING AVG(Rate) > 15
