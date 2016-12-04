0c25931e6d30d90136478755734045b9e0096a43
Ox Cart <ox.to.a.cart@gmail.com>
2016-12-04 07:27:21 -0600

    Fixed limit

--- core/limit.go
+++ core/limit.go
@@ -17,7 +17,7 @@ func (l *Limit) Iterate(onRow OnFlatRow) error {
 		newIdx := atomic.AddInt64(&idx, 1)
 		oldIdx := int(newIdx - 1)
 		// TODO: allow stopping iteration here
-		if oldIdx >= l.Offset && l.Limit == 0 || oldIdx < l.Limit+l.Offset {
+		if oldIdx >= l.Offset && (l.Limit == 0 || oldIdx < l.Limit+l.Offset) {
 			onRow(row)
 		}
 	})
