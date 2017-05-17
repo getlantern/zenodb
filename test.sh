2a0844290a0b580c7d39b913d979e6a4f67728f1
Ox Cart <ox.to.a.cart@gmail.com>
2017-05-17 08:23:04 -0500

    Added coverage reporting

--- test.sh
+++ test.sh
@@ -1,3 +1,9 @@
 #! /usr/bin/env sh
-find . -type d | grep -v vendor | grep -v ".git" | xargs -I{} echo "(cd {}; go test) &&" | paste -s -d" " - | sed 's/$/ true/' | /usr/bin/env sh
+echo "mode: count" > profile.cov
+TP=$(find . -name "*_test.go" -printf '%h\n' | grep  -v vendor | grep -v glide | sort -u)
+set -x && \
+for pkg in $TP; do \
+	go test -v -tags="headless" -covermode=atomic -coverprofile=profile_tmp.cov $pkg || exit 1; \
+	tail -n +2 profile_tmp.cov >> profile.cov; \
+done
 exit $?
