a4b07f3170bcc882471ff5f677c1d036ff5635f8
Ox Cart <ox.to.a.cart@gmail.com>
2020-06-13 01:38:59 -0500

    Back to evaluating where in distributing to followers

--- insert.go
+++ insert.go
@@ -41,6 +41,9 @@ func (db *DB) InsertRaw(stream string, ts time.Time, dims bytemap.ByteMap, vals 
 	encoding.WriteInt32(dimsLen, len(dims))
 	valsLen := make([]byte, encoding.Width32bits)
 	encoding.WriteInt32(valsLen, len(vals))
+	if db.log.IsTraceEnabled() {
+		db.log.Tracef("Writing to wal with dims: %v", bytemap.ByteMap(dims).AsMap())
+	}
 	err := w.Write(tsd, dimsLen, dims, valsLen, vals)
 	if err != nil {
 		db.log.Error(err)
